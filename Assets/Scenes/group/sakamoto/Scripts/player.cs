a26cbc886f88a609e1fe38a122175fc3d2f86094
竜之助 <pokemon0116@i.softbank.jp>
2026-03-11 15:10:04 +0900

    重なり判定追加

---
+++ Assets/Scenes/group/sakamoto/Scripts/player.cs
@@ -0,0 +1,51 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private RectTransform targetRectTransform;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 addPos = new Vector3(0.0f, 0.0f, 0.0f);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            addPos.y = 15.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            addPos.y = -15.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            addPos.x = 15.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            addPos.x = -15.0f;
+        }
+
+        transform.position = transform.position + (addPos * Time.deltaTime);
+    
+        //èdÇ»Ç¡ÇƒÇ¢ÇÈÇ©
+        if(rectTransform.IsOverLapping(targetRectTransform))
+        {
+            Debug.Log("Ç†ÇΩÇ¡ÇΩ");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ìñÇΩÇ¡ÇΩ");
+    }
+}

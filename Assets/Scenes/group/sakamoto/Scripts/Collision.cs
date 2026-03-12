b2b0f41d4f9717b52b5466df2c8d7389710fdc8a
OotaniMao <daguzhensheng@gmail.com>
2026-03-12 13:54:51 +0900

    sakamotoのscripts変更

--- Assets/Scenes/group/sakamoto/Scripts/Collision.cs
+++ Assets/Scenes/group/sakamoto/Scripts/Collision.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private RectTransform[] targetRectTransform;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +15,18 @@ public class Collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //èdÇ»Ç¡ÇƒÇ¢ÇÈÇ©
+        for (int i = 0; i < targetRectTransform.Length; i++)
+        {
+            if (rectTransform.IsOverLapping(targetRectTransform[i]))
+            {
+                Debug.Log("Ç†ÇΩÇ¡ÇΩ");
+            }
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ìñÇΩÇ¡ÇΩ");
+    }
 }

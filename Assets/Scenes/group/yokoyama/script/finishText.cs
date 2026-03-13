b4498f4cdf8da6e1aa73cd6b3100440154f0c124
yomotchi4091 <yomogi.4091@gmail.com>
2026-03-13 14:24:40 +0900

    fiishアニメーション

--- Assets/Scenes/group/yokoyama/script/finishText.cs
+++ Assets/Scenes/group/yokoyama/script/finishText.cs
@@ -8,26 +8,43 @@ public class finishTimer : MonoBehaviour
     [SerializeField] private MoveObject moveObject;
     [SerializeField] private SceneTransitionButton sceneManager;
 
+    private const float FONT_SIZE_MAX = 100f;
+    private const float FONT_SIZE_MIN = 20f;
+    private const float FONT_SIZE_CHANGE_SPEED = -0.5f;
+    private float fontSize = FONT_SIZE_MIN;
+
     private void Start()
     {
 
         countDownText.text = "";
-
+        fontSize = FONT_SIZE_MAX;
+ 
     }
 
     private void Update()
     {
 
-       if(moveObject.GetIsFinished())
+       if(moveObject.GetIsFinished() == true)
        {
            countDownText.text = "finish!";
-            sceneManager.IsInTitle = true;
+          
+            fontSize += FONT_SIZE_CHANGE_SPEED;
+
+            if (fontSize < FONT_SIZE_MIN)
+            {
+
+                fontSize = FONT_SIZE_MIN;
+                sceneManager.IsInTitle = true;
+
+            }
        }
        else
        {
            countDownText.text = "";
-        }
+       }
 
+        countDownText.fontSize = fontSize;
+    
     }
 
 }
